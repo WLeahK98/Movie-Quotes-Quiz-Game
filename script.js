e3b4f911b75fb15edcc98e1263a74919ec40b565
Wehlia <wehliakahin@gmail.com>
2025-08-20 16:48:46 +0100

    Quiz Completed!
    Added show score fuction to display score text at the end of the game.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*
+Javascript code for quotes quiz. The quiz has four questions. Each question has a prompt and a list of possible answers.
+
+*/
+
 
 const questions = [
     {
@@ -10,9 +15,9 @@ const questions = [
         ]
     },
     {
-        question: "If you don’t like your destiny, don’t accept it. Instead, have the courage to change it!",
+        question: "If you don't like your destiny, dont accept it. Instead, have the courage to change it!",
         answers: [
-            { text: "Attack on Titan", correct: false },
+            { text: "Demon Slayer", correct: false },
             { text: "Naruto", correct: true },
             { text: "Lord of the Rings", correct: false },
             { text: "John Wick", correct: false },
@@ -23,7 +28,7 @@ const questions = [
         question: "If you are feeling disheartened, that you are somehow not enough, SET YOUR HEART ABLAZE!",
         answers: [
             { text: "Demon Slayer", correct: true },
-            { text: "Naruto", correct: true },
+            { text: "Naruto", correct: false },
             { text: "Lord of the Rings", correct: false },
             { text: "John Wick", correct: false },
         ]
@@ -33,7 +38,7 @@ const questions = [
         question: "People keep asking if I'm back and I havent really had an answer but now yeah, I'M THINKING I'M BACK!",
         answers: [
             { text: "Demon Slayer", correct: false },
-            { text: "Naruto", correct: true },
+            { text: "Naruto", correct: false },
             { text: "Lord of the Rings", correct: false },
             { text: "John Wick", correct: true },
         ]
@@ -47,6 +52,9 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
+/*
+ Starts the quiz: resets indices and score and shows the first question.
+ */
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
@@ -72,7 +80,7 @@ it will also display the answers text within each button*/
             button.dataset.correct = answer.correct;
         }
         button.addEventListener("click", selectAnswer);
-    })
+    });
 }
 
 function resetState(){
@@ -82,15 +90,67 @@ function resetState(){
     }
 }
 
+
+/*
+Called when an answer button is clicked. Applies styling to indicate correctness.
+*/
 function selectAnswer(e){
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+/*score++ will increase the score by 1 if the correct answer is selected*/
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
+/*
+Added array button from answerButtons.children. The forEacg button will check the dataset, if it is true,
+the it will add the class name "correct". Suppose we have selected the wrong answer, then it'll go and check the
+other answers and if it is true, it will add the green color.
+*/  Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        } 
+/*After it will diable the buttons and we cant click on other buttons*/
+        button.disabled = true;
+    });
+/*This will display the next button, allowing us to go to the next question*/
+    nextButton.style.display = "block";
 }
+
+
+/*showScore fuction will display out of the length of questions*/
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+
+}
+
+/*Defining handleNextButton fuction*/
+function handleNextButton(){
+    currentQuestionIndex++; /*Will increase the currentQuestion index by 1, when we click on the next button*/
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+/*Function for the next button*/
+nextButton.addEventListener("click", () => {
+/*Checks to see: if the current question index is less than the length of the question. EX:
+if the question is question 4, checks to see if the index is less than 4.
+*/
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
 
 /*Calling the startQuiz function an will set the current index to 0, score to 0 and display button text.
 Then it wil call the showQuestions function*/
